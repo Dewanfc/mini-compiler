7e2b0d921bdfabaef4c40ac1a51ac83d06e561cd
Dewan <150391268+Dewanfc@users.noreply.github.com>
2026-07-30 18:23:52 +0600

    added additional features by modifying ast.h file

--- src/ast/ast.c
+++ src/ast/ast.c
@@ -5,34 +5,46 @@
 #include "ast.h"
 
 
+
 ASTNode* create_node(NodeType type)
 {
     ASTNode *node = (ASTNode*)malloc(sizeof(ASTNode));
 
+
     if(node == NULL)
     {
-        fprintf(stderr, "Memory allocation failed\n");
+        fprintf(stderr,"Memory allocation failed\n");
         exit(1);
     }
 
 
+
     node->type = type;
+
 
     node->name = NULL;
 
+
     node->int_val = 0;
+
     node->float_val = 0.0;
 
 
+
     node->left = NULL;
+
     node->right = NULL;
+
     node->third = NULL;
 
     node->next = NULL;
 
 
+
     return node;
 }
+
+
 
 
 
@@ -40,10 +52,14 @@ ASTNode* create_identifier(char *name)
 {
     ASTNode *node = create_node(NODE_IDENTIFIER);
 
+
     node->name = strdup(name);
+
 
     return node;
 }
+
+
 
 
 
@@ -51,10 +67,14 @@ ASTNode* create_int_const(int value)
 {
     ASTNode *node = create_node(NODE_INT_CONST);
 
+
     node->int_val = value;
+
 
     return node;
 }
+
+
 
 
 
@@ -62,25 +82,194 @@ ASTNode* create_float_const(float value)
 {
     ASTNode *node = create_node(NODE_FLOAT_CONST);
 
+
     node->float_val = value;
+
 
     return node;
 }
 
 
 
+
+
+
+/*
+    Constant Folding
+
+    Example:
+
+    create_binop("+",
+                 create_int_const(10),
+                 create_int_const(20))
+
+    becomes:
+
+    create_int_const(30)
+
+*/
+
 ASTNode* create_binop(char *op,
                       ASTNode *left,
                       ASTNode *right)
 {
+
+    if(left != NULL &&
+       right != NULL &&
+       left->type == NODE_INT_CONST &&
+       right->type == NODE_INT_CONST)
+    {
+
+        int result = 0;
+
+
+
+        if(strcmp(op,"+")==0)
+        {
+            result = left->int_val + right->int_val;
+        }
+
+
+        else if(strcmp(op,"-")==0)
+        {
+            result = left->int_val - right->int_val;
+        }
+
+
+        else if(strcmp(op,"*")==0)
+        {
+            result = left->int_val * right->int_val;
+        }
+
+
+        else if(strcmp(op,"/")==0)
+        {
+            if(right->int_val != 0)
+                result = left->int_val / right->int_val;
+            else
+                return NULL;
+        }
+
+
+        else if(strcmp(op,"%")==0)
+        {
+            result = left->int_val % right->int_val;
+        }
+
+
+        else if(strcmp(op,"<")==0)
+        {
+            result = left->int_val < right->int_val;
+        }
+
+
+        else if(strcmp(op,">")==0)
+        {
+            result = left->int_val > right->int_val;
+        }
+
+
+        else if(strcmp(op,"==")==0)
+        {
+            result = left->int_val == right->int_val;
+        }
+
+
+        else
+        {
+            goto normal_node;
+        }
+
+
+
+        return create_int_const(result);
+    }
+
+
+
+normal_node:
+
+
     ASTNode *node = create_node(NODE_BINOP);
+
 
 
     node->name = strdup(op);
 
+
     node->left = left;
 
     node->right = right;
+
+
+
+    return node;
+}
+
+
+
+
+
+
+
+
+/*
+    Unary operator creation
+
+    Examples:
+
+    -x
+
+    !x
+
+*/
+
+ASTNode* create_unary(char *op,
+                      ASTNode *child)
+{
+
+    /*
+        Constant folding for unary operators
+
+        -10  ---> -10
+
+        !0   ---> 1
+    */
+
+
+    if(child != NULL &&
+       child->type == NODE_INT_CONST)
+    {
+
+        if(strcmp(op,"-")==0)
+        {
+            return create_int_const(
+                -(child->int_val)
+            );
+        }
+
+
+        if(strcmp(op,"!")==0)
+        {
+            return create_int_const(
+                !(child->int_val)
+            );
+        }
+
+    }
+
+
+
+
+    ASTNode *node = create_node(NODE_UNARY);
+
+
+
+    node->name = strdup(op);
+
+
+    node->left = child;
+
 
 
     return node;
