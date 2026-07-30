d6855ee72616182490d1591548e71c97c7e5b0da
Dewan <150391268+Dewanfc@users.noreply.github.com>
2026-07-30 18:53:16 +0600

    added tac.c with the updated features

--- src/semantic/tac.c
+++ src/semantic/tac.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 #include "../ast/ast.h"
 
 
+
 int temp_count = 1;
+
 int label_count = 1;
 
 
@@ -36,6 +37,7 @@ char *new_label()
 
 char *generate_tac(ASTNode *node)
 {
+
     if(node == NULL)
         return NULL;
 
@@ -45,355 +47,429 @@ char *generate_tac(ASTNode *node)
     {
 
 
-    case NODE_INT_CONST:
-    {
-        char *temp = new_temp();
-
-        printf("%s = %d\n",
-               temp,
-               node->int_val);
-
-        return temp;
-    }
-
-
-
-    case NODE_FLOAT_CONST:
-    {
-        char *temp = new_temp();
-
-        printf("%s = %f\n",
-               temp,
-               node->float_val);
-
-        return temp;
-    }
-
-
-
-
-    case NODE_IDENTIFIER:
-    {
-        return node->name;
-    }
-
-
-
-
-    case NODE_BINOP:
-    {
-        char *left =
-            generate_tac(node->left);
-
-
-        char *right =
-            generate_tac(node->right);
-
-
-        char *temp =
-            new_temp();
-
-
-
-        if(right != NULL)
+        case NODE_INT_CONST:
         {
+            char *temp = new_temp();
+
+            printf("%s = %d\n",
+                   temp,
+                   node->int_val);
+
+
+            return temp;
+        }
+
+
+
+
+        case NODE_FLOAT_CONST:
+        {
+            char *temp = new_temp();
+
+            printf("%s = %f\n",
+                   temp,
+                   node->float_val);
+
+
+            return temp;
+        }
+
+
+
+
+
+        case NODE_IDENTIFIER:
+        {
+            return node->name;
+        }
+
+
+
+
+
+        case NODE_BINOP:
+        {
+            char *left =
+                generate_tac(node->left);
+
+
+            char *right =
+                generate_tac(node->right);
+
+
+
+            char *temp = new_temp();
+
+
+
             printf("%s = %s %s %s\n",
                    temp,
                    left,
                    node->name,
                    right);
+
+
+
+            return temp;
         }
-        else
+
+
+
+
+
+
+        /*
+            Unary operator TAC
+
+            Example:
+
+            x = -10
+
+            becomes:
+
+            t1 = -10
+            x = t1
+
+
+            !x
+
+            becomes:
+
+            t2 = !x
+
+        */
+
+        case NODE_UNARY:
         {
-            printf("%s = %s %s\n",
+
+            char *value =
+                generate_tac(node->left);
+
+
+
+            char *temp =
+                new_temp();
+
+
+
+            printf("%s = %s%s\n",
                    temp,
                    node->name,
-                   left);
+                   value);
+
+
+
+            return temp;
+
         }
 
 
-        return temp;
-    }
-
-
-
-
-    case NODE_ASSIGN:
-    {
-        char *value =
-            generate_tac(node->right);
-
-
-
-        printf("%s = %s\n",
-               node->left->name,
-               value);
-
-
-        return node->left->name;
-    }
-
-
-
-
-    case NODE_PRINT:
-    {
-        char *value =
-            generate_tac(node->left);
-
-
-
-        if(value != NULL)
-            printf("print %s\n", value);
-
-
-        return NULL;
-    }
 
 
 
 
 
-
-    case NODE_BLOCK:
-    {
-        ASTNode *current = node->left;
-
-
-        while(current != NULL)
+        case NODE_ASSIGN:
         {
-            generate_tac(current);
 
-            current = current->next;
+            char *value =
+                generate_tac(node->right);
+
+
+
+            printf("%s = %s\n",
+                   node->left->name,
+                   value);
+
+
+
+            return node->left->name;
         }
 
 
-        return NULL;
-    }
 
 
 
 
 
 
+        case NODE_PRINT:
+        {
 
-    case NODE_IF:
-    {
-        char *condition =
-            generate_tac(node->left);
+            char *value =
+                generate_tac(node->left);
 
 
-        char *true_label =
-            new_label();
 
+            if(value != NULL)
+            {
+                printf("print %s\n",
+                       value);
+            }
 
-        char *end_label =
-            new_label();
 
 
+            return NULL;
+        }
 
-        printf("if %s goto %s\n",
-               condition,
-               true_label);
 
 
-        printf("goto %s\n",
-               end_label);
 
 
 
-        printf("%s:\n",
-               true_label);
 
 
+        case NODE_BLOCK:
+        {
 
-        generate_tac(node->right);
+            ASTNode *current =
+                node->left;
 
 
 
-        printf("%s:\n",
-               end_label);
+            while(current != NULL)
+            {
 
+                generate_tac(current);
 
 
-        return NULL;
-    }
+                current=current->next;
 
+            }
 
 
+            return NULL;
 
+        }
 
 
 
-    case NODE_IF_ELSE:
-    {
-        char *condition =
-            generate_tac(node->left);
 
 
 
-        char *true_label =
-            new_label();
 
 
-        char *false_label =
-            new_label();
+        case NODE_IF:
+        {
 
+            char *condition =
+                generate_tac(node->left);
 
-        char *end_label =
-            new_label();
 
 
+            char *label_true =
+                new_label();
 
 
+            char *label_end =
+                new_label();
 
-        printf("if %s goto %s\n",
-               condition,
-               true_label);
 
 
+            printf("if %s goto %s\n",
+                   condition,
+                   label_true);
 
-        printf("goto %s\n",
-               false_label);
 
 
+            printf("goto %s\n",
+                   label_end);
 
 
 
-        printf("%s:\n",
-               true_label);
+            printf("%s:\n",
+                   label_true);
 
 
 
-        generate_tac(node->right);
-
-
-
-        printf("goto %s\n",
-               end_label);
-
-
-
-
-
-        printf("%s:\n",
-               false_label);
-
-
-
-        generate_tac(node->third);
-
-
-
-
-
-        printf("%s:\n",
-               end_label);
-
-
-
-        return NULL;
-    }
-
-
-
-
-
-
-
-
-    case NODE_WHILE:
-    {
-        char *start =
-            new_label();
-
-
-        char *body =
-            new_label();
-
-
-        char *end =
-            new_label();
-
-
-
-
-
-        printf("%s:\n",
-               start);
-
-
-
-        char *condition =
-            generate_tac(node->left);
-
-
-
-
-        printf("if %s goto %s\n",
-               condition,
-               body);
-
-
-
-        printf("goto %s\n",
-               end);
-
-
-
-
-
-        printf("%s:\n",
-               body);
-
-
-
-        generate_tac(node->right);
-
-
-
-        printf("goto %s\n",
-               start);
-
-
-
-        printf("%s:\n",
-               end);
-
-
-
-        return NULL;
-    }
-
-
-
-
-
-
-
-    default:
-    {
-
-        if(node->left)
-            generate_tac(node->left);
-
-
-        if(node->right)
             generate_tac(node->right);
 
 
-        if(node->third)
+
+            printf("%s:\n",
+                   label_end);
+
+
+
+            return NULL;
+
+        }
+
+
+
+
+
+
+
+
+        case NODE_IF_ELSE:
+        {
+
+            char *condition =
+                generate_tac(node->left);
+
+
+
+            char *label_true =
+                new_label();
+
+
+            char *label_false =
+                new_label();
+
+
+            char *label_end =
+                new_label();
+
+
+
+
+            printf("if %s goto %s\n",
+                   condition,
+                   label_true);
+
+
+
+            printf("goto %s\n",
+                   label_false);
+
+
+
+
+            printf("%s:\n",
+                   label_true);
+
+
+
+            generate_tac(node->right);
+
+
+
+            printf("goto %s\n",
+                   label_end);
+
+
+
+
+            printf("%s:\n",
+                   label_false);
+
+
+
             generate_tac(node->third);
 
 
-        if(node->next)
-            generate_tac(node->next);
+
+
+            printf("%s:\n",
+                   label_end);
 
 
 
-        return NULL;
-    }
+            return NULL;
 
+        }
+
+
+
+
+
+
+
+
+
+        case NODE_WHILE:
+        {
+
+            char *start =
+                new_label();
+
+
+            char *body =
+                new_label();
+
+
+            char *end =
+                new_label();
+
+
+
+
+            printf("%s:\n",
+                   start);
+
+
+
+            char *condition =
+                generate_tac(node->left);
+
+
+
+            printf("if %s goto %s\n",
+                   condition,
+                   body);
+
+
+
+            printf("goto %s\n",
+                   end);
+
+
+
+            printf("%s:\n",
+                   body);
+
+
+
+            generate_tac(node->right);
+
+
+
+            printf("goto %s\n",
+                   start);
+
+
+
+            printf("%s:\n",
+                   end);
+
+
+
+            return NULL;
+
+        }
+
+
+
+
+
+
+        default:
+        {
+
+            if(node->left)
+                generate_tac(node->left);
+
+
+
+            if(node->right)
+                generate_tac(node->right);
+
+
+
+            if(node->third)
+                generate_tac(node->third);
+
+
+
+            if(node->next)
+                generate_tac(node->next);
+
+
+
+            return NULL;
+
+        }
 
 
     }
