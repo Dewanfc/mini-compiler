84b60837d9263df69532578db1eab42f95df383c
Dewan <150391268+Dewanfc@users.noreply.github.com>
2026-07-30 17:59:10 +0600

    modified ast.h

--- src/ast/ast.h
+++ src/ast/ast.h
@@ -1,27 +1,69 @@
 #ifndef AST_H
 #define AST_H
 
-typedef enum {
-    NODE_VAR_DECL, NODE_ASSIGN, NODE_BINOP, NODE_IDENTIFIER, 
-    NODE_INT_CONST, NODE_FLOAT_CONST, NODE_BOOL_CONST, 
-    NODE_IF, NODE_WHILE, NODE_PRINT, NODE_BLOCK
+typedef enum
+{
+    NODE_PROGRAM,
+
+    NODE_VAR_DECL,
+    NODE_ASSIGN,
+
+    NODE_BINOP,
+
+    NODE_IDENTIFIER,
+    NODE_INT_CONST,
+    NODE_FLOAT_CONST,
+
+    NODE_PRINT,
+
+    NODE_BLOCK,
+
+    NODE_IF,
+    NODE_IF_ELSE,
+
+    NODE_WHILE
+
 } NodeType;
 
-typedef struct ASTNode {
+
+
+typedef struct ASTNode
+{
     NodeType type;
-    char *name;             // For identifiers and operators
-    int int_val;            // For int constants
-    float float_val;        // For float constants
-    int bool_val;           // For boolean constants
-    
-    struct ASTNode *left;   // Left child (or condition)
-    struct ASTNode *right;  // Right child (or body/else)
-    struct ASTNode *next;   // For lists of statements
+
+    char *name;
+
+    int int_val;
+    float float_val;
+
+
+    struct ASTNode *left;
+    struct ASTNode *right;
+
+    // Used for else block in if-else
+    struct ASTNode *third;
+
+
+    // Linked list of statements
+    struct ASTNode *next;
+
+
 } ASTNode;
 
+
+
 ASTNode* create_node(NodeType type);
-ASTNode* create_binop(char* op, ASTNode* left, ASTNode* right);
-ASTNode* create_identifier(char* name);
-ASTNode* create_int_const(int val);
+
+ASTNode* create_identifier(char *name);
+
+ASTNode* create_int_const(int value);
+
+ASTNode* create_float_const(float value);
+
+ASTNode* create_binop(char *op,
+                      ASTNode *left,
+                      ASTNode *right);
+
+
 
 #endif
